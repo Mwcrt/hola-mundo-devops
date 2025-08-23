a577c52cb20d85935e330425f8d2b787616e57cf
Mwcrt <miguelacostasrc@gmail.com>
2025-08-22 20:57:57 -0400

    Update index.js and server

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require('./index');
+
 const PORT = process.env.PORT || 3000;
 
+// Escuchar el puerto y mantener el proceso vivo
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+// Evitar que Node termine si hay errores no capturados
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:', err);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err);
 });
