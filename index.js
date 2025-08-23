a1b0845725bbee742b3026bc3e72dad7b03d4d44
Mwcrt <miguelacostasrc@gmail.com>
2025-08-22 20:52:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
   res.send('Hola Mundo desde DevOps CI/CD!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
 module.exports = app;
