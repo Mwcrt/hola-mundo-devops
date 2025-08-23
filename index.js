40ea7ad02123f688cdc131f243f4b9bd0dfcf1cc
Mwcrt <miguelacostasrc@gmail.com>
2025-08-22 20:44:45 -0400

    Proyecto listo: tests, Docker y workflow CI/CD

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const request = require('supertest');
-const app = require('./index');
+const express = require('express');
+const app = express();
 
-let server;
-
-beforeAll(() => {
-  server = app.listen(3000); // levantar servidor solo para tests
+app.get('/', (req, res) => {
+  res.send('Hola Mundo desde DevOps CI/CD!');
 });
 
-afterAll((done) => {
-  server.close(done); // cerrar servidor al finalizar pruebas
-});
-
-describe('GET /', () => {
-  it('Debe devolver Hola Mundo', async () => {
-    const res = await request(server).get('/');
-    expect(res.text).toBe('Hola Mundo desde DevOps CI/CD!');
-    expect(res.statusCode).toBe(200);
-  });
-});
+module.exports = app; // exporta la app sin levantar el servidor
--- index.test.js
+++ index.test.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const app = express();
+const request = require('supertest');
+const app = require('./index');
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo desde DevOps CI/CD!');
+let server;
+let port;
+
+beforeAll(() => {
+  port = 3000 + Math.floor(Math.random() * 1000); // puerto aleatorio 3000-3999
+  server = app.listen(port);
 });
 
-module.exports = app; // exporta la app sin levantar el servidor
+afterAll((done) => {
+  server.close(done);
+});
+
+describe('GET /', () => {
+  it('Debe devolver Hola Mundo', async () => {
+    const res = await request(`http://localhost:${port}`).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hola Mundo desde DevOps CI/CD!');
+  });
+});
